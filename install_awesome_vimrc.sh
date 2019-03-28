bab0fd4143b4ccecafe30560af8e4fbd782d4813
raghavgai <raghavgaibond@gmail.com>
2019-03-28 08:03:15 +0530

    Updated with my customizations

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -10,6 +10,11 @@ source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 
+" raghav - customize start
+set nu
+set background=light
+" raghav - customize end
+
 try
 source ~/.vim_runtime/my_configs.vim
 catch
